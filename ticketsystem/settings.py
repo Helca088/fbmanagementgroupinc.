907cf81d04ff9f5417e929d9f35d6c2cf3b5ee39
Helca088 <noweaponallowed@gmail.com>
2026-06-14 14:56:59 +0800

    fix allowed hosts

--- ticketsystem/settings.py
+++ ticketsystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=0mxswoxp_!r-pq2li)l9!tr%#1-744ehj9&a*w&0-+0yi=-4j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fbmanagementgroupinc.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
