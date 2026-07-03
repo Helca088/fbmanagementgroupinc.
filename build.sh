d34fc8c3592c5fd3df2a01a7ef8502f42671a3c5
Helca088 <noweaponallowed@gmail.com>
2026-07-03 17:21:07 +0800

    Add section field to Technician

--- build.sh
+++ build.sh
@@ -5,3 +5,15 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
 python manage.py migrate --run-syncdb
 python manage.py migrate
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        "admin",
+        "admin@example.com",
+        "YourPassword123!"
+    )
+EOF
