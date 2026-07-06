11e4c0de88ffcbd1b765ccd1683fb541ed3b98f8
Helca088 <noweaponallowed@gmail.com>
2026-07-06 23:04:05 +0800

    fcm config fix

--- tickets/apps.py
+++ tickets/apps.py
@@ -1,4 +1,6 @@
 # apps.py
+import os
+import json
 from django.apps import AppConfig
 import firebase_admin
 from firebase_admin import credentials
@@ -8,5 +10,8 @@ class TicketsConfig(AppConfig):
 
     def ready(self):
         if not firebase_admin._apps:
-            cred = credentials.Certificate("serviceAccountKey.json")
+            cred_json = os.environ.get("FIREBASE_CREDENTIALS_JSON")
+            if not cred_json:
+                raise RuntimeError("FIREBASE_CREDENTIALS_JSON environment variable is not set")
+            cred = credentials.Certificate(json.loads(cred_json))
             firebase_admin.initialize_app(cred)
