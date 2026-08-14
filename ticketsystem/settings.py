9a2158af2735cf941d4b045830f866608a955ec7
Helca088 <noweaponallowed@gmail.com>
2026-08-14 15:27:11 +0800

    Fixing CSRF

--- ticketsystem/settings.py
+++ ticketsystem/settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'channels',
     'tickets',
     'cloudinary',
@@ -112,6 +113,7 @@ UNFOLD = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware", 
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
